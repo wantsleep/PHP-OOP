d02bc65577fa496db346240e9266528b2fd80474
test <wantsleep2001@yahoo.com.tw>
2016-06-28 14:04:47 +0800

    VER1

---
+++ ClassTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class Cat {
+
+    public $name;
+    public $position;
+
+    public function __construct($name) {
+
+        $this->name = $name;
+        $this->position = ['x' => 0, 'y' => 0];
+    }
+
+}
+?>
+
+<!doctype html>
+<html lang="zh-tw">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+
+    <body>
+        <?php ?>
+    </body>
+</html>
